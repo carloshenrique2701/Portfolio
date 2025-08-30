f5c9c5167454b89b77fe61949f8919252ee03bfd
carloshenrique2701 <carloshrr1209@gmail.com>
2025-08-30 19:07:11 -0300

    Correção de responsividade

--- Projetos/Projeto 1 - Alternador de Cores/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('botao').addEventListener('click', function() {
-    
-    const corAleatoria = Math.floor(Math.random() * 1562473).toString(16);
-
-    document.body.style.background = "#" + corAleatoria;
-
-});
--- Projetos/Projeto 28 - FAQ/script.js
+++
@@ -1,9 +0,0 @@
-function alternarResposta(elemento) {
-    
-    const resposta = elemento.querySelector('.resposta');
-
-    resposta.classList.toggle('mostrar');
-
-    resposta.classList.toggle('escondido');
-
-}
